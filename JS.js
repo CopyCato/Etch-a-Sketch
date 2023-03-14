bf869be80a104bc21c7c716f4d9561d4fdef13bb
CopyCato <nikola93nikolov@gmail.com>
2023-03-14 12:58:41 +0100

    Created 16x16 grid

---
+++ JS.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.grid-container');
+for(let i = 0; i < 256; i++){
+  const div = document.createElement('div');
+  div.classList.add('grid-item');
+  container.appendChild(div);
+}
