cc86e7b93675831dd11b852652f0c4536adf0d10
CopyCato <nikola93nikolov@gmail.com>
2023-03-14 13:47:30 +0100

    Add hover effect

--- JS.js
+++ JS.js
@@ -1,6 +1,14 @@
 const container = document.querySelector('.grid-container');
+
 for(let i = 0; i < 256; i++){
   const div = document.createElement('div');
   div.classList.add('grid-item');
   container.appendChild(div);
 }
+
+const hover = document.querySelectorAll('.grid-item');
+hover.forEach((item) => {
+  item.addEventListener('mouseover', function(e){
+    e.target.style.backgroundColor = "blue";
+  });
+});
