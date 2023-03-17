8e83efab918317f40e275be887f0e126d6264448
CopyCato <nikola93nikolov@gmail.com>
2023-03-17 16:33:26 +0100

    Finish

--- JS.js
+++ JS.js
@@ -1,14 +1,47 @@
 const container = document.querySelector('.grid-container');
+const button = document.querySelector('button');
+let gridSize = 16;
+let autoString = '';
 
-for(let i = 0; i < 256; i++){
-  const div = document.createElement('div');
-  div.classList.add('grid-item');
-  container.appendChild(div);
+function createGrid(gridSize){
+  for(let i = 0; i < gridSize; i++){
+    autoString += 'auto ';
+    for(let j = 0; j < gridSize; j++){
+      const div = document.createElement('div');
+      div.classList.add('grid-item');
+      container.appendChild(div);
+    }
+  }
+  container.style.gridTemplateColumns = autoString;
 }
 
-const hover = document.querySelectorAll('.grid-item');
-hover.forEach((item) => {
-  item.addEventListener('mouseover', function(e){
-    e.target.style.backgroundColor = "blue";
+function deleteGrid(){
+  const element = document.querySelectorAll('.grid-item ');
+  element.forEach((item) => {
+    item.remove();
   });
+}
+
+function hoverEffect(){
+  const hover = document.querySelectorAll('.grid-item');
+  hover.forEach((item) => {
+    item.addEventListener('mouseover', function(e){
+      e.target.style.backgroundColor = "blue";
+    });
+  });
+}
+
+createGrid(gridSize);
+hoverEffect();
+
+
+button.addEventListener('click', () => {
+  deleteGrid(gridSize);
+  autoString = '';
+  let newGridSize = 0;
+  newGridSize = prompt('Number of squares per side for the new grid');
+  if(newGridSize < 100){
+    createGrid(newGridSize);
+    hoverEffect();
+  }
 });
